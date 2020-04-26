e159f0f0f43acb79dab7f729a16b507a1107916d
Ali Younes <ay02-5-1994@hotmail.com>
2020-04-26 21:07:48 +0300

    Update install_opnecv_linux.sh

--- install_opnecv_linux.sh
+++ install_opnecv_linux.sh
@@ -1,7 +1,12 @@
 sudo apt-get install -y build-essential
 sudo apt-get install -y cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+sudo apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev
 
+sudo add-apt-repository "deb http://security.ubuntu.com/ubuntu xenial-security main"
+sudo apt update
+sudo apt install libjasper1 libjasper-dev
+
+rm -r opencv_install
 mkdir opencv_install
 # shellcheck disable=SC2164
 cd opencv_install
